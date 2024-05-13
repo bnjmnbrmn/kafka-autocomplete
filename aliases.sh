8a87126b324615604197ad6b56a46b3b2f9a7967
Benjamin Berman <baberman@mmm.com>
2024-05-13 13:45:44 -0400

    Add aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,3 @@
+alias kt='kafka-topics --bootstrap-server localhost:9092'
+alias kcc='kafka-console-consumer --bootstrap-server localhost:9092'
+alias kcp='kafka-console-producer --bootstrap-server localhost:9092'
